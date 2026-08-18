bfb04e4f331a9b8c986c5586fda1221d33395ff8
vivekreddy-03 <vivekreddy3033@gmail.com>
2026-08-18 17:22:17 +0000

    .

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "Vivek@gmail.com"
+var accountPassword = "12345"
+accountCity = "Hyderabad"
+let accountState;
+
+// accountId = 2 // (not allowed)
+accountEmail = "Keviv@gmail.com"
+accountPassword = "23456"
+accountCity = "Banglore"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
